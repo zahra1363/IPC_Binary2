c18cf2fdf4a62d0d023c4ddd3666d58cfe5e7d46
zahra1363 <zahra.ghahvechi@cm-robotics.com>
2023-02-16 14:16:06 +0100

    finally IPC-Binary

--- file4.cpp
+++ file4.cpp
@@ -2,11 +2,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <bitset>
+#include <fstream>
 
 int main(int argc, char **argv)
 {
-    char * mypipe= (char*)"/home/solmaz/Documents/intership c++ code/mypipe"; // Change to your desired pipe name
-    const char* text_file = (char*)"/home/solmaz/Documents/intership c++ code/text_file.txt"; 
+    char *mypipe = (char *)"/home/solmaz/Documents/intership c++ code/IPC_Binary2/mypipe"; // Change to your desired pipe name
+    const char *text_file = (char *)"/home/solmaz/Documents/intership c++ code/IPC_Binary2/text_file.txt";
 
     // Open the named pipe for reading
     int read_fd = open(mypipe, O_RDONLY);
@@ -17,7 +18,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
-   // Read the binary data from the named pipe
+    // Read the binary data from the named pipe
     char binary_buffer[256];
     ssize_t binary_size = read(read_fd, binary_buffer, 256);
 
@@ -37,12 +38,22 @@ int main(int argc, char **argv)
     std::cout << std::endl;
 
     // converting char array to equivalent integer
-    std::cout << " the integer value of the binary number is: ";
+    std::cout << "The integer value of the binary number is: ";
     std::string binary_string(binary_buffer);    // Convert the char array to a std::string
     std::bitset<8> binary_number(binary_string); // Convert the binary string to a binary number using std::bitset
-
     std::cout << binary_number.to_ulong();
     std::cout << std::endl;
+
+    std::cout << "Binary data is writting in the text file ..." << std::endl;
+    // Open the file for writing
+    std::ofstream outfile;
+    outfile.open(text_file);
+
+    // Write the binary to the file
+    outfile << binary_buffer;
+
+    // Close the file
+    outfile.close();
 
     // Close the named pipe for reading
     close(read_fd);
